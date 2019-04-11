f3044d0ba55b0eb667fc4b4e6192138dfd688364
doojin kim <doojin88@gmail.com>
2019-04-11 15:46:34 +0900

    create maven project

---
+++ src/main/java/kr/leo/yaml/leoyaml/LeoYamlApplication.java
@@ -0,0 +1,13 @@
+package kr.leo.yaml.leoyaml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeoYamlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeoYamlApplication.class, args);
+	}
+
+}
